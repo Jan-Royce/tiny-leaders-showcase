93d389a9573814ff59c9a2149d3b92c419f59576
Jan Royce Maceren <jroyce@osolabs.com>
2025-07-21 12:32:52 +0800

    Fix mobile issue and add copy to clipboard function for button

--- app/page.jsx
+++ app/page.jsx
@@ -756,8 +756,19 @@ const upcoming = [
 ];
 
 export default function Page() {
-    function copyLink() {
-        alert('WIP');
+    function copyLink(e) {
+        const btn = e.currentTarget;
+        const { name, link } = btn.dataset;
+        
+        let tempText = document.createElement("input");
+        tempText.value = link;
+        document.body.appendChild(tempText);
+        
+        tempText.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempText);
+        
+        alert(`Copied Moxfield link for ${name}`);
     }
     
     return (
@@ -848,13 +859,15 @@ export default function Page() {
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
-                                        {deck.url}
+                                        Moxfield
                                     </Link>
                                     
                                     <button 
                                         type="button" 
-                                        className="copy-link text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-auto mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-max-content cursor-pointer"
+                                        className="copy-deck-link text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-auto mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-max-content cursor-pointer"
                                         onClick={copyLink}
+                                        data-name={deck.name}
+                                        data-link={deck.url}
                                     >
                                         Copy Link
                                     </button>
